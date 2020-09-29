fae3b3aa7a75e8b374f796dfde2685d6b3cdd0de
OrangeX4 <318483724@qq.com>
2020-09-29 21:44:35 +0800

    Complete truthtable

---
+++ 离散数学/Homework/truthtable.py
@@ -0,0 +1,84 @@
+def imply(p, q):
+    if not p:
+        return True
+    elif q:
+        return True
+    else:
+        return False
+
+
+def onlyif(p, q):
+    if p and q:
+        return True
+    elif not p and not q:
+        return True
+    else:
+        return False
+
+
+def xor(p, q):
+    return not onlyif(p, q)
+
+
+def boolToString(p):
+    if p:
+        return 'T'
+    else:
+        return 'F'
+
+
+def truthtable(func, *set):
+    # Init input
+    input = {}
+    for i in range(0, len(set)):
+        input[set[i]] = False
+
+    def block(begin, end):
+
+        if begin < end - 1:
+            current = set[begin]
+            input[current] = False
+            block(begin + 1, end)
+
+            input[current] = True
+            block(begin + 1, end)
+        else:
+            def printOut():
+
+                out = ''
+                for i in range(0, end):
+                    out = out + '|' + boolToString(input[set[i]]) + ' '
+                out = out + '|'+ boolToString(func(tuple(input.values()))) +' |'
+
+                print(out)
+            
+            current = set[begin]
+            input[current] = False
+            printOut()
+
+            input[current] = True
+            printOut()
+
+            return
+
+    out = ''
+    for i in range(0, len(set)):
+        out = out + '|' + set[i] + ' '
+    out = out + '|'
+    print(out)
+
+    out = ''
+    for i in range(0, len(set)):
+        out = out + '|--'
+    out = out + '|'
+    print(out)
+
+    block(0, len(set) - 1)
+
+
+
+def function(p):
+    return p[0] and p[1]
+
+
+truthtable(function, 'p', 'q', 'Ans')

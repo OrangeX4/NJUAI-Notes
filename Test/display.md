616690dceb05d407bfb3a0f94e19583b467be9a6
OrangeX4 <318483724@qq.com>
2021-05-13 11:32:01 +0800

    Complete Test 5

--- Test/display.md
+++ Test/display.md
@@ -1,3 +1,16 @@
-$$
+$x_1+\alpha x_1=x_{1} \left(\alpha + 1\right)$
 
-$$
+$\sin{\frac{\pi}{2}}=1$
+
+$\frac{d}{dx}(x^3+x^2+1)=x \left(3 x + 2\right)$
+
+$\int x^2=\frac{x^{3}}{3}$
+
+$\begin{vmatrix} x &1 \\ 0 &1 \\\end{vmatrix}=x$
+
+$\begin{bmatrix} 1 & 1 \\ 0& 1\\\end{bmatrix}^{-1}=\left[\begin{matrix}1 & -1\\0 & 1\end{matrix}\right]$
+
+$\begin{bmatrix} 1 & 1 \\ 0& 1\\\end{bmatrix}^{-1}\begin{bmatrix} 1 & 1 \\ 0& 1\\\end{bmatrix}=\left[\begin{matrix}1 & 0\\0 & 1\end{matrix}\right]$
+
+$\begin{bmatrix} 1 & 1 \\ 0& 1\\\end{bmatrix}\xrightarrow{r_1\leftrightarrow r_2}=\left[\begin{matrix}0 & 1\\1 & 1\end{matrix}\right]$
+
--- 智能程序/Exam/test.py
+++ 智能程序/Exam/test.py
@@ -1,63 +1,37 @@
-class Matrix:
-    def __init__(self, n, m, fill=0, matrix=None, stroke=(0, 0)) -> None:
-        self.shape_r = n
-        self.shape_c = m
-        if matrix:
-            self.matrix = matrix
-            self.stroke_r = stroke[0]
-            self.stroke_c = stroke[1]
+class InfSequence:
+
+    def __init__(self, start, end, step: int = 1):
+        self._start = start
+        self._end = end
+        self._step: int = step
+        self._current = start - step
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self._current += self._step
+        if self._step == 0 and self._start == self._end:
+            raise StopIteration
+        elif self._step == 0 and self._start != self._end:
+            return self._current
+        elif self._step > 0:
+            if self._end == 'inf':
+                return self._current
+            elif self._end == '-inf':
+                raise StopIteration
+            elif self._current < self._end:
+                return self._current
+            else:
+                raise StopIteration
         else:
-            self.matrix = [fill] * (n * m)
-            self.stroke_r = m
-            self.stroke_c = 1
-
-    def print(self):
-        print('--------')
-        print('matrix', self.matrix)
-        print('Stroke:', self.stroke_r, self.stroke_c)
-        for r in range(self.shape_r):
-            for c in range(self.shape_c):
-                print(self.matrix[(r) * self.stroke_r +
-                              (c) * self.stroke_c], end=' ')
-            print('')
-        print('--------')
-
-    def set_value(self, pos, value):
-        try:
-            r, c = pos
-            if r > self.shape_r or r < 1 or c > self.shape_c or c < 1:
-                raise ValueError('越界')
-            self.matrix[(r - 1) * self.stroke_r +
-                        (c - 1) * self.stroke_c] = value
-        except Exception as e:
-            print('设置元素下标越界')
-
-    def get_value(self, pos):
-        try:
-            r, c = pos
-            if r > self.shape_r or r < 1 or c > self.shape_c or c < 1:
-                raise ValueError('越界')
-            print(self.matrix[(r - 1) * self.stroke_r +
-                              (c - 1) * self.stroke_c])
-        except Exception as e:
-            print('获取元素下标越界')
-
-    def transpose(self):
-        return Matrix(self.shape_c, self.shape_r, matrix=self.matrix.copy(), stroke=(self.stroke_c, self.stroke_r))
-
-    def reshape(self, shape):
-        r, c = shape
-        if self.shape_r * self.shape_c != r * c:
-            print('不能将该矩阵转为目标形状')
-            return(None)
-        if self.stroke_c == 1:
-            return Matrix(r, c, matrix=self.matrix.copy(), stroke=(c, 1))
-        else:
-            m = Matrix(r, c)
-            for i in range(self.shape_r):
-                for j in range(self.shape_c):
-                    m.matrix[i * self.shape_c + j] = self.matrix[i * self.stroke_r + j * self.stroke_c]
-            return m
+            if self._end == '-inf':
+                return self._current
+            elif self._current > self._end:
+                return self._current
+            else:
+                raise StopIteration
 
 
-exec(input())
+while True:
+    exec(input())

5086dc2ebc705183a5d64af1722843ed30ad5a70
OrangeX4 <318483724@qq.com>
2021-10-12 01:18:42 +0800

    Complete Homework

---
+++ Test/display.md
@@ -0,0 +1,10 @@
+
+排列组合:
+
+$\displaystyle (5)_{3}=60$
+
+$\displaystyle \frac{\displaystyle \sum_{i=1}^{5}\binom{5}{i}(3-1)^{5-i}}{10^{5}}=\frac{211}{100000}$
+
+无穷级数:
+
+$\displaystyle \sum_{k=0}^{\infty}\binom{n}{k}x^{k}=\begin{cases} \left(x + 1\right)^{n} & \text{for}\: \left(\operatorname{re}{\left(n\right)} \leq -1 \vee \left|{x}\right| \leq 1\right) \wedge \left(\left|{x}\right| \leq 1 \vee \left|{x}\right| < 1\right) \wedge \left(\operatorname{re}{\left(n\right)} > -1 \vee \operatorname{re}{\left(n\right)} > 0 \vee \left|{x}\right| < 1\right) \wedge \left(\operatorname{re}{\left(n\right)} \leq -1 \vee \operatorname{re}{\left(n\right)} > 0 \vee x \neq -1\right) \wedge \left(\operatorname{re}{\left(n\right)} > 0 \vee \left|{x}\right| < 1 \vee x \neq -1\right) \\\sum_{k=0}^{\infty} x^{k} {\binom{n}{k}} & \text{otherwise} \end{cases}$
--- Test/test.py
+++ Test/test.py
@@ -1,37 +1,5 @@
-import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA 
+from latex2sympy2 import *
+from sympy import *
 
-# 1. Loading dataset into Pandas DataFrame and get numpy data
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
-data = df.iloc[:, :4].to_numpy()
-# data = np.array([[-1., -1., -1.], [-2., -1., 5.], [-3., -2., 3.], [1., 1., 2.], [2., 1., 4.], [3., 2., 3.]])
-
-def pca(data, n_component):
-    data = data.T
-
-    # 2. Subtract the mean
-    data -= data.mean(axis = 1).reshape(-1, 1)
- 
-    # 3. Get the covariance matrix: 1/m * XX^T
-    cov_matrix = 1 / len(data[0]) * data @ data.T
- 
-    # 4. Calculate the eigenvalue and eigenvector
-    eigenvalues, eigenmatrix = np.linalg.eig(cov_matrix)
-
-    # 5. Get data after dimensionality reduction and Sort eigenmatrix by eigenvalues
-    new_data = eigenmatrix.T @ data
-
-    # 6. Sort data
-    return new_data[sorted(range(len(eigenvalues)),  key=lambda v: -eigenvalues[v])][:n_component]
-
-new_data = pca(data, 2)
-
-print(new_data.T)
-
-pca=PCA(n_components=2)
-new_data = pca.fit_transform(data)
-
-print(new_data)
+for k in range(0, 3):
+    print(latex2sympy(r"\frac{\binom{2}{k}\binom{13}{3-k}}{\binom{15}{3}}").subs({Symbol("k"): k}).doit())

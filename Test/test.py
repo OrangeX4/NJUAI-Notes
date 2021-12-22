72d79d676d626bec55e22afd1627bc2d43b0c852
OrangeX4 <318483724@qq.com>
2021-12-22 23:32:09 +0800

    Complete PS12

--- Test/test.py
+++ Test/test.py
@@ -1,5 +1,22 @@
-from latex2sympy2 import *
-from sympy import *
+A = 'ARTISTOIL'
+n = len(A)
 
-for k in range(1, 5):
-    print(latex2sympy(r"\frac{\displaystyle \sum_{i=1}^{3}\binom{3}{i}(4-k)^{3-i}}{4^{3}}").subs({Symbol("k"): k}).doit())
+def IsWord(s):
+    return s == 'ART' or s == 'IS' or s == 'TOIL' or s == 'ARTIST' or s == 'OIL'
+
+# 用于动态规划的数组
+p = [0 for _ in range(n + 1)]
+
+# 初始化 p[0]
+p[0] = 1
+
+# 主循环
+for k in range(1, n + 1):
+    sum = 0
+    for i in range(0, k):
+       if IsWord(A[i : k]):
+           sum += p[i]
+    p[k] = sum
+
+# 输出最后结果
+print(p[n])

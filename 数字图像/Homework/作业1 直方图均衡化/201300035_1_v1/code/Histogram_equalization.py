7b27b7af8f1de08efd3f58da8cc00c51fc9e2754
OrangeX4 <318483724@qq.com>
2022-09-29 16:25:06 +0800

    Complete basic hsi function

--- 数字图像/Homework/作业1 直方图均衡化/201300035_1_v1/code/Histogram_equalization.py
+++ 数字图像/Homework/作业1 直方图均衡化/201300035_1_v1/code/Histogram_equalization.py
@@ -25,14 +25,12 @@ class Image:
         # 初始化其他成员变量
         self._hist = None
 
-    
     def get_image(self) -> np.ndarray:
         '''
         获取图像矩阵
         '''
         return self._image
 
-    
     def show(self) -> None:
         '''
         显示图像
@@ -41,7 +39,6 @@ class Image:
         plt.imshow(self._image, cmap='gray' if self._is_gray_image else None)
         plt.show()
 
-    
     def compare_with(self, before: Image) -> None:
         '''
         与前一张图像进行对比显示
@@ -51,24 +48,25 @@ class Image:
         # 显示两张图像
         plt.subplot(1, 2, 1)
         plt.title('Before')
-        plt.imshow(before.get_image(), cmap='gray' if before._is_gray_image else None)
+        plt.imshow(before.get_image(),
+                   cmap='gray' if before._is_gray_image else None)
         plt.subplot(1, 2, 2)
         plt.title('After')
-        plt.imshow(self.get_image(), cmap='gray' if self._is_gray_image else None)
+        plt.imshow(self.get_image(),
+                   cmap='gray' if self._is_gray_image else None)
         plt.show()
-
 
     @staticmethod
     def get_hist_from_gray_image(gray_image: np.ndarray, *, gray_level=256) -> np.ndarray:
         '''
         获取灰度图像的直方图, 返回一个一维数组, 灰阶默认为 256, 数组的每个元素对应图像的每个灰度级的像素个数
         '''
-        hist = np.zeros(gray_level)
-        for i in range(gray_image.shape[0]):
-            for j in range(gray_image.shape[1]):
-                hist[gray_image[i][j]] += 1
+        # hist = np.zeros(gray_level)
+        # for i in range(gray_image.shape[0]):
+        #     for j in range(gray_image.shape[1]):
+        #         hist[gray_image[i][j]] += 1
+        hist = np.bincount(gray_image.flatten(), minlength=gray_level)
         return hist
-
 
     def hist(self) -> np.ndarray:
         '''
@@ -79,15 +77,16 @@ class Image:
             return self._hist
         # 如果未初始化, 则进行计算
         if self._is_gray_image:
-            self._hist = self.get_hist_from_gray_image(self._image, gray_level=self._gray_level)
+            self._hist = self.get_hist_from_gray_image(
+                self._image, gray_level=self._gray_level)
         else:
             # RGB 分别处理
             self._hist = []
             for i in range(3):
-                self._hist.append(self.get_hist_from_gray_image(self._image[:, :, i], gray_level=self._gray_level))
+                self._hist.append(self.get_hist_from_gray_image(
+                    self._image[:, :, i], gray_level=self._gray_level))
             self._hist = np.array(self._hist)
         return self._hist
-
 
     def show_hist(self) -> None:
         '''
@@ -112,7 +111,6 @@ class Image:
             plt.title('Blue')
             plt.bar(range(self._gray_level), self.hist()[2], color='blue')
         plt.show()
-
 
     def compare_hist_with(self, before: Image):
         '''
@@ -152,7 +150,6 @@ class Image:
             plt.bar(range(self._gray_level), self.hist()[2], color='blue')
         plt.show()
 
-    
     @staticmethod
     def histeq_for_gray_image(image, *, gray_level=256) -> np.ndarray:
         '''
@@ -168,15 +165,74 @@ class Image:
         # 计算映射函数
         s = np.zeros(gray_level)
         for i in range(gray_level):
-            s[i] = round((gray_level - 1) * cum_hist[i] / (image.shape[0] * image.shape[1]))
+            s[i] = round((gray_level - 1) * cum_hist[i] /
+                         (image.shape[0] * image.shape[1]))
         # 映射
-        new_image = np.zeros(image.shape, dtype=np.uint8 if gray_level <= 256 else np.uint16)
+        new_image = np.zeros(
+            image.shape, dtype=np.uint8 if gray_level <= 256 else np.uint16)
         for i in range(image.shape[0]):
             for j in range(image.shape[1]):
                 new_image[i][j] = s[image[i][j]]
         return new_image
 
-    
+    @staticmethod
+    def RGB2HSI(image: np.ndarray, *, gray_level=256) -> np.ndarray:
+        '''
+        将 RGB 图像矩阵转换为 HSI 图像矩阵 (强度和饱和度会被转化为 0 到 1 之间的浮点数)
+        1. H = theta if B <= G else 360 - theta
+           theta = arccos(0.5 * (R - G + R - B) / sqrt((R - G)^2 + (R - B) * (G - B)))
+        2. S = 1 - 3 / (R + G + B) * min(R, G, B)
+        3. I = (R + G + B) / 3
+        '''
+        # RGB 矩阵
+        R, G, B = image[:, :, 0] / (gray_level - 1), image[:, :, 1] / \
+            (gray_level - 1), image[:, :, 2] / (gray_level - 1)
+        # 1. 计算 Hue 色调分量, 分母加上一个小分量避免除 0
+        theta = np.arccos(0.5 * (R - G + R - B) /
+                          (np.sqrt((R - G) ** 2 + (R - B) * (G - B)) + 1e-10))
+        H = np.where(B <= G, theta, 2 * np.pi - theta)
+        # 2. 计算 Saturation 饱和度分量
+        S = 1 - 3 / (R + G + B) * np.min([R, G, B], axis=0)
+        # 3. 计算 Intensity 强度分量
+        I = (R + G + B) / 3
+        # 综合
+        HSI = np.dstack([H, S, I])
+        return HSI
+
+    @staticmethod
+    def HSI2RGB(image: np.ndarray, *, gray_level=256) -> np.ndarray:
+        '''
+        将 HSI 图像矩阵转换为 RGB 图像矩阵
+        1. 位于 RG 扇区 (0 <= H < 120) 时:
+           B = I * (1 - S)
+           R = I * (1 + S * cos(H) / cos(60 - H))
+           G = 3 * I - (R + B)
+        2. 位于 GB 扇区 (120 <= H < 240) 时:
+           R = I * (1 - S)
+           G = I * (1 + S * cos(H - 120) / cos(180 - H))
+           B = 3 * I - (R + G)
+        3. 位于 BR 扇区 (240 <= H < 360) 时:
+           G = I * (1 - S)
+           B = I * (1 + S * cos(H - 240) / cos(300 - H))
+           R = 3 * I - (G + B)
+        '''
+        # HSI 矩阵
+        H, S, I = image[:, :, 0], image[:, :, 1], image[:, :, 2]
+        # 计算 RGB 分量
+        R = np.where(H < 2 / 3 * np.pi, I * (1 + S * np.cos(H) / (np.cos(np.pi / 3 - H) + 1e-10)),
+                     np.where(H < 4 / 3 * np.pi, I * (1 - S),
+                              3 * I - (I * (1 - S) + I * (1 + S * np.cos(H - 4 / 3 * np.pi) / (np.cos(5 * np.pi / 3 - H) + 1e-10)))))
+        G = np.where(H < 2 / 3 * np.pi, 3 * I - (I * (1 - S) + I * (1 + S * np.cos(H) / (np.cos(np.pi / 3 - H) + 1e-10))),
+                     np.where(H < 4 / 3 * np.pi, I * (1 + S * np.cos(H - 2 / 3 * np.pi) / (np.cos(np.pi - H) + 1e-10)),
+                              I * (1 - S)))
+        B = np.where(H < 2 / 3 * np.pi, I * (1 - S),
+                     np.where(H < 4 / 3 * np.pi, 3 * I - (I * (1 - S) + I * (1 + S * np.cos(H - 2 / 3 * np.pi) / (np.cos(np.pi - H) + 1e-10))),
+                              I * (1 + S * np.cos(H - 4 / 3 * np.pi) / (np.cos(5 * np.pi / 3 - H) + 1e-10))))
+        # 综合
+        RGB = (np.dstack([R, G, B]) * (gray_level - 1)
+               ).astype(np.uint8 if gray_level <= 256 else np.uint16)
+        return RGB
+
     def histeq(self, *, method='gray') -> Image:
         '''
         进行图像直方图均衡化, 计算公式为 s = T(r) = round((L-1) * sum(p[0:r]) / N * M)
@@ -185,16 +241,19 @@ class Image:
             2. HSI 空间直方图均衡化
         '''
         if self._is_gray_image:
-            new_image = self.histeq_for_gray_image(self._image, gray_level=self._gray_level)
+            new_image = self.histeq_for_gray_image(
+                self._image, gray_level=self._gray_level)
         else:
             if method == 'gray':
                 # 1. 分通道灰度图像直方图均衡化
-                new_image = np.zeros(self._image.shape, dtype=np.uint8 if self._gray_level <= 256 else np.uint16)
+                new_image = np.zeros(
+                    self._image.shape, dtype=np.uint8 if self._gray_level <= 256 else np.uint16)
                 for i in range(3):
-                    new_image[:, :, i] = self.histeq_for_gray_image(self._image[:, :, i], gray_level=self._gray_level)
+                    new_image[:, :, i] = self.histeq_for_gray_image(
+                        self._image[:, :, i], gray_level=self._gray_level)
             elif method == 'hsi':
                 # 2. HSI 空间直方图均衡化
-                raise NotImplementedError
+                new_image = self.HSI2RGB(self.RGB2HSI(self._image))
             else:
                 raise ValueError('method must be "gray" or "hsi"')
         return Image(new_image, gray_level=self._gray_level)
@@ -222,9 +281,15 @@ if __name__ == '__main__':
     # img.histeq().compare_hist_with(img)
 
     img = Image(mpimg.imread('../asset/image/boat.jpg'))
-    img.histeq().compare_with(img)
-    img.histeq().compare_hist_with(img)
+    # img.histeq().compare_with(img)
+    # img.histeq().compare_hist_with(img)
+    img.histeq(method='hsi').compare_with(img)
+    img.histeq(method='hsi').compare_hist_with(img)
 
     # img = Image(mpimg.imread('../asset/image/genshin.jpg'))
+    # img.histeq().compare_with(img)
+    # img.histeq().compare_hist_with(img)
+
+    # img = Image(mpimg.imread('../asset/image/night.jpg'))
     # img.histeq().compare_with(img)
     # img.histeq().compare_hist_with(img)

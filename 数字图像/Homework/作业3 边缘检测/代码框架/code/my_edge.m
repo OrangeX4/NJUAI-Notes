df22e8ab0b39b308d453d9d2f5b851d71b8681e2
OrangeX4 <318483724@qq.com>
2022-12-07 10:44:47 +0800

    Add Homework

---
+++ 数字图像/Homework/作业3 边缘检测/代码框架/code/my_edge.m
@@ -0,0 +1,8 @@
+function output = my_edge(input_image)
+%in this function, you should finish the edge detection utility.
+%the input parameter is a matrix of a gray image
+%the output parameter is a matrix contains the edge index using 0 and 1
+%the entries with 1 in the matrix shows that point is on the edge of the
+%image
+%you can use different methods to complete the edge detection function
+%the better the final result and the more methods you have used, you will get higher scores  
---
+++ 数字图像/Homework/作业3 边缘检测/代码框架/code/my_edgelinking.m
@@ -0,0 +1,10 @@
+function output = my_edgelinking(binary_image, row, col)
+%in this function, you should finish the edge linking utility.
+%the input parameters are a matrix of a binary image containing the edge
+%information and coordinates of one of the edge points of a obeject
+%boundary, you should run this function multiple times to find different
+%object boundaries
+%the output parameter is a Q-by-2 matrix, where Q is the number of boundary 
+%pixels. B holds the row and column coordinates of the boundary pixels.
+%you can use different methods to complete the edge linking function
+%the better the quality of object boundary and the more the object boundaries, you will get higher scores  
